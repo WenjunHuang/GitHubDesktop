24a7e0bad84651a4298290c2fa278e392fd46371
WenjunHuang <wenjun.huang80@gmail.com>
2019-10-18 19:08:34 +0800

    user viewmodel

--- app/python/desktop/lib/viewmodels/changes/changes_view_model.py
+++ app/python/desktop/lib/viewmodels/changes/changes_view_model.py
@@ -1,24 +1,35 @@
 from enum import IntEnum
 from typing import Dict
 
-from PyQt5.QtCore import QAbstractItemModel
+from PyQt5.QtCore import QAbstractItemModel, Qt, QModelIndex
 from PyQt5.QtQuick import QQuickItem
+
+from desktop.lib.git.status import WorkingDirectoryFileChange
 
 
 class Roles(IntEnum):
-    Selected, File, Kind = range(3)
+    Include = Qt.UserRole + 1
+    File = Qt.UserRole + 2
+    Kind = Qt.UserRole + 3
 
 
 class ChangeFileList(QAbstractItemModel):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+        self._list = []
 
     def roleNames(self) -> Dict[int, 'QByteArray']:
         roles = dict()
-        roles[Roles.Selected.value] = 'selected'
+        roles[Roles.Include.value] = 'selected'
         roles[Roles.File.value] = 'file'
         roles[Roles.Kind.value] = 'kind'
+
+    def rowCount(self, parent: QModelIndex) -> int:
+        return len(self._list)
+
+    def add_change_file(self,change_file:WorkingDirectoryFileChange):
+        self.beginInsertRows()
 
 
 class ChangesViewModel(QQuickItem):
